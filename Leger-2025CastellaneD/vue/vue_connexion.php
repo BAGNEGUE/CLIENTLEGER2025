442bd1af302f61306c82d1b4dbea84cbe923d412
bruxie <bagneguechristb@gmail.com>
2025-04-24 22:54:57 +0200

    Ajout du code du site castellane autoecole

---
+++ Leger-2025CastellaneD/vue/vue_connexion.php
@@ -0,0 +1,20 @@
+<h3> Connexion au Site Auto Ecole Castellane </h3>
+<img src="images/logor.png" height="100" width="100">
+
+<form method="post">
+	<table>
+		<tr>
+			<td> EMAIL</td>
+			<td><input type="text" name="email"></td>
+		</tr>
+		<tr>
+			<td> MDP</td>
+			<td><input type="password" name="mdp"></td>
+		</tr>
+		<tr>
+			<td> <input type="reset" name="Annuler" value="Annuler"></td>
+			<td><input type="submit" name="Connexion" value="Connexion"></td>
+		</tr>
+	</table>
+	
+</form>
---
+++ Leger-2025CastellaneD/vue/vue_footer.php
@@ -0,0 +1,14 @@
+<div id="footer">
+    <p id="contact">Contact : castellane@gmail.com</p> <!-- Contact en haut -->
+    <ul>
+        <li> <a href=""> Site Officiel </a> </li>
+        <li> <a href="index.php?page=2"> Les Candidats </a> </li>
+        <li> <a href="index.php?page=3"> Les Moniteurs </a> </li>
+        <li> <a href="index.php?page=4"> Les Responsables </a> </li>
+        <li> <a href="index.php?page=5"> Les Véhicules </a> </li>
+        <li> <a href="index.php?page=6"> Les Leçons </a> </li>
+        <li> <a href="index.php?page=7"> Planning </a> </li>
+        <li> <a href="index.php?page=8"> Les Examens </a> </li>
+    </ul>
+    <p id="copyright">Tous droits réservés à l'Auto Ecole Castellane - 2025.</p>
+</div>
